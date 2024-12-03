109d9c3354ff1a6539956f4bc8bb26ce781752ba
Jo Vinkenroye <jestersimpps@gmail.com>
2024-12-03 16:49:57 +0100

    feat: Add solid background color to ImageModal for better visibility

--- src/components/ImageModal.tsx
+++ src/components/ImageModal.tsx
@@ -15,7 +15,7 @@ export default function ImageModal({ imageUrl, alt, isOpen, onClose }: ImageModa
       className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center p-4"
       onClick={onClose}
     >
-      <div className="relative max-w-[90vw] max-h-[90vh]">
+      <div className="relative max-w-[90vw] max-h-[90vh] bg-white dark:bg-gray-900 p-4 rounded-lg">
         <button
           onClick={onClose}
           className="absolute -top-4 -right-4 bg-white dark:bg-gray-800 rounded-full p-2 hover:bg-gray-100 dark:hover:bg-gray-700"
